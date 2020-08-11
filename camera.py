77361c4e45459f6d840aefb9cc04aca4d169cae0
Kunal Dutta <kunaldutta1749@gmail.com>
2020-08-11 15:08:02 -0700

    reverted to original with opencv

--- camera.py
+++ camera.py
@@ -3,17 +3,20 @@ import cv2
 
 class Camera:
     def __init__(self):
+        # Initialize camera.
         self.vs = cv2.VideoCapture()
-        self.detections = 0
 
         # Let camera warm up for 5 seconds.
         print("Starting camera...")
         time.sleep(5)
 
     def __del__(self):
+        # Clean up data.
         self.vs.release()
         cv2.destroyAllWindows()
 
     def get_frame(self):
-        self.vs.grab()
-        return self.vs.retrieve()
+        # Grab frame from video capture and return its bytes.
+        frame = self.vs.read()
+        buf = cv2.imencode('.jpg', frame)[1].tostring()
+        return buf
