20114d9ae1ea67443cfd7aec3a44d7aeae690759
Kunal Dutta <kunaldutta1749@gmail.com>
2020-08-11 15:15:38 -0700

    print image array

--- camera.py
+++ camera.py
@@ -18,5 +18,6 @@ class Camera:
     def get_frame(self):
         # Grab frame from video capture and return its bytes.
         frame = self.vs.read()
+        print(frame)
         buf = cv2.imencode('.jpg', frame)[1].tostring()
         return buf
