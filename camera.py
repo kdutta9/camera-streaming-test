93baa84690c56e07692bb6773dc73a6418fada58
Kunal Dutta <kdutta@berkeley.edu>
2020-08-11 16:29:16 -0700

    Access frames with OpenCV

--- camera.py
+++ camera.py
@@ -4,11 +4,14 @@ import cv2
 class Camera:
     def __init__(self):
         # Initialize camera.
-        self.vs = cv2.VideoCapture()
+        self.vs = cv2.VideoCapture(0)
 
         # Let camera warm up for 5 seconds.
         print("Starting camera...")
         time.sleep(5)
+
+        if not self.vs.isOpened():
+            print("Cannot open webcam.")
 
     def __del__(self):
         # Clean up data.
@@ -17,6 +20,6 @@ class Camera:
 
     def get_frame(self):
         # Grab frame from video capture and return its bytes.
-        frame = self.vs.read()
+        frame = self.vs.read()[1]
         buf = cv2.imencode('.jpg', frame)[1].tostring()
         return buf
